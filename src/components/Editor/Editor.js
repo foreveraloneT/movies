114da18c006ad22516b32dccbf1271b1be191f4e
Sutee Suriyaruengwet <sutee.s@enegist.com>
2017-05-17 17:50:47 +0700

    update editor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,6 +2,11 @@ import React from 'react';
 import styles from './Editor.css'
 import { EditorContent } from './EditorContent'
 
+const getValue = (e) => {
+    const title = document.getElementById('title')
+    console.log(title.innerText)
+}
+
 export const Editor = ({
     
 }) => (
@@ -11,13 +16,14 @@ export const Editor = ({
                className={`${styles["title-space"]} no-focus`}
                contentEditable={true}
                id="title"
-               placeholder="Title here..." >
+               placeholder="Title here..."
+               onKeyDown={(e) => getValue(e)} >Title here...
             </div>
             <div 
                 className={`${styles["content-space"]} no-focus`}
                 contentEditable={true}
                 id="content"
-                placeholder="Review here..." >
+                placeholder="Review here..." >Review here...
             </div>
         </div>
         <div className="col-md-4">
